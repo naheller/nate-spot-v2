eb3f685775daf4660ae321a91c0eec2592450336
Nathan Heller <gritty.desk3330@fastmail.com>
2026-02-09 16:05:55 +0100

    handling internal links, tags, and notes index

--- meta.lua
+++ meta.lua
@@ -1,13 +1,5 @@
 function Meta(meta)
-    local filename = pandoc.utils.stringify(PANDOC_STATE.input_files)
-    local title = filename:match("([^/]+)%.")
-    local slug = title:lower():gsub(" ", "-")
-    io.write(slug)
-
-    if meta.title == nil then
-        -- Use filename as title in metadata
-        meta.title = title
-    end
+    -- local filename = pandoc.utils.stringify(PANDOC_STATE.input_files)
 
     if meta.tags and #meta.tags > 0 then
         local tags_list = {}
@@ -28,7 +20,22 @@ function Meta(meta)
 end
 
 -- Convert internal links to .html
--- function Link(el)
---   el.target = string.gsub(el.target, "%.md", ".html")
---   return el
--- end
+function Link(el)
+    -- Only slugify if link is not pointing to external URL.
+    -- Must always include http when inserting external links in markdown.
+    if string.find(el.target, "^" .. "http") == nil then
+        local slug = slugify(pandoc.utils.stringify(el.content))
+        el.target = "/" .. slug
+    end
+
+    return el
+end
+
+function slugify(str)
+    str = str:lower()
+    str = str:gsub("['’]", "") -- remove apostrophes
+    str = str:gsub("[^a-z0-9]+", "-") -- replace non-alphanumerics with hyphens
+    str = str:gsub("^-+", ""):gsub("-+$", "") -- trim leading/trailing hyphens
+
+    return str
+end
